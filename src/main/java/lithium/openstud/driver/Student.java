df54cc8e84baa3782e37c3715605377b4c4d7864
Leonardo Sarra <leonardosarra@outlook.com>
2018-05-26 22:23:53 +0200

    Student: Add student model

---
+++ src/main/java/lithium/openstud/driver/Student.java
@@ -0,0 +1,26 @@
+package lithium.openstud.driver;
+
+public class Student {
+    private String CF;
+    private String firstName;
+    private String lastName;
+    private String birthDate;
+    private String birthCity;
+    private String courseYear;
+    private String firstEnrollment;
+    private String lastEnrollment;
+    private String departmentName;
+    private String courseName;
+    private String nation;
+    private int academicYear;
+    private int academicYearCourse;
+    private int studentID;
+    private int codeCourse;
+    private char typeStudent;
+    private char gender;
+    private boolean inTime;
+    private boolean isErasmus;
+
+    private void Student(){
+    }
+}
