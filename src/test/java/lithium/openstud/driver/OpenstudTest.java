e52c1be10b10b4eaa24e37c368ecc6dd6177e7b1
Leonardo Sarra <leonardosarra@outlook.com>
2018-05-27 00:24:30 +0200

    Test: Add a test for getIsee

--- src/test/java/lithium/openstud/driver/OpenstudTest.java
+++ src/test/java/lithium/openstud/driver/OpenstudTest.java
@@ -5,12 +5,19 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 
-public class OpenstudAuthTest
+public class OpenstudTest
 {
     @Test
     public void testLogin() throws OpenstudInvalidUserException, OpenstudEndpointNotReadyException, OpenstudInvalidPasswordException, OpenstudConnectionException {
         Openstud osb = new OpenstudBuilder().setPassword(System.getenv("OPENSTUD_TESTPWD")).setStudentID(Integer.parseInt(System.getenv("OPENSTUD_TESTID"))).build();
         osb.login();
         assertTrue( osb.isReady() );
+    }
+
+    @Test
+    public void testGetIsee() throws OpenstudInvalidUserException, OpenstudEndpointNotReadyException, OpenstudInvalidPasswordException, OpenstudConnectionException, OpenstudInvalidSetupException {
+        Openstud osb = new OpenstudBuilder().setPassword(System.getenv("OPENSTUD_TESTPWD")).setStudentID(Integer.parseInt(System.getenv("OPENSTUD_TESTID"))).build();
+        osb.login();
+        assertTrue(osb.getIsee().isValid());
     }
 }
