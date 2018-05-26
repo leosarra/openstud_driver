5e591f893666c80a0900c560e08369c3fb65b9f8
Leonardo Sarra <leonardosarra@outlook.com>
2018-05-27 00:18:33 +0200

    OpenStud: Throw invalidSetupException if OpenStud object is not ready

---
+++ src/main/java/lithium/openstud/driver/OpenstudInvalidSetupException.java
@@ -0,0 +1,7 @@
+package lithium.openstud.driver;
+
+public class OpenstudInvalidSetupException extends Exception {
+    public OpenstudInvalidSetupException(String message) {
+        super(message);
+    }
+}
