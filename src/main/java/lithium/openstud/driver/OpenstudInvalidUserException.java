618b32d02a38cebb415335fbdb8fed6ea7b488b7
Leonardo Sarra <leonardosarra@outlook.com>
2018-05-27 12:10:58 +0200

    Exception: Removed invalidUserException

--- src/main/java/lithium/openstud/driver/OpenstudInvalidUserException.java
+++
@@ -1,7 +0,0 @@
-package lithium.openstud.driver;
-
-public class OpenstudInvalidUserException extends Exception {
-    public OpenstudInvalidUserException(String message) {
-        super(message);
-    }
-}
