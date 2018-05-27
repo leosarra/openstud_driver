d4008f7dec25c5396a4a3ee7f7fd7471b287db89
Leonardo Sarra <leonardosarra@outlook.com>
2018-05-27 22:45:24 +0200

    Test: Add tests for getExamsDoable(),getExamsPassed(),getActiveReservations()

--- src/test/java/lithium/openstud/driver/OpenstudTest.java
+++ src/test/java/lithium/openstud/driver/OpenstudTest.java
@@ -1,5 +1,6 @@
 package lithium.openstud.driver;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import lithium.openstud.driver.exceptions.OpenstudConnectionException;
@@ -7,6 +8,7 @@ import lithium.openstud.driver.exceptions.OpenstudEndpointNotReadyException;
 import lithium.openstud.driver.exceptions.OpenstudInvalidPasswordException;
 import lithium.openstud.driver.exceptions.OpenstudInvalidResponseException;
 import org.junit.Test;
+import java.util.List;
 
 
 public class OpenstudTest
@@ -32,5 +34,29 @@ public class OpenstudTest
         osb.login();
         Student st=osb.getInfoStudent();
         assertTrue(st!=null && st.getStudentID()!=0);
+    }
+
+    @Test
+    public void testGetExamsDoable() throws OpenstudEndpointNotReadyException, OpenstudInvalidResponseException, OpenstudInvalidPasswordException, OpenstudConnectionException {
+        Openstud osb = new OpenstudBuilder().setPassword(System.getenv("OPENSTUD_TESTPWD")).setStudentID(Integer.parseInt(System.getenv("OPENSTUD_TESTID"))).build();
+        osb.login();
+        List<ExamDoable> list=osb.getExamsDoable();
+        assertNotNull(list);
+    }
+
+    @Test
+    public void testGetExamsPassed() throws OpenstudEndpointNotReadyException, OpenstudInvalidResponseException, OpenstudInvalidPasswordException, OpenstudConnectionException {
+        Openstud osb = new OpenstudBuilder().setPassword(System.getenv("OPENSTUD_TESTPWD")).setStudentID(Integer.parseInt(System.getenv("OPENSTUD_TESTID"))).build();
+        osb.login();
+        List<ExamPassed> list=osb.getExamsPassed();
+        assertNotNull(list);
+    }
+
+    @Test
+    public void testGetActiveReservations() throws OpenstudEndpointNotReadyException, OpenstudInvalidResponseException, OpenstudInvalidPasswordException, OpenstudConnectionException {
+        Openstud osb = new OpenstudBuilder().setPassword(System.getenv("OPENSTUD_TESTPWD")).setStudentID(Integer.parseInt(System.getenv("OPENSTUD_TESTID"))).build();
+        osb.login();
+        List<ExamReservation> list=osb.getActiveReservations();
+        assertNotNull(list);
     }
 }
